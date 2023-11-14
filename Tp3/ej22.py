494fb7fc6c50e30adfc046b8e483d7a72038ac46
Elluis1 <105747004+Elluis1@users.noreply.github.com>
2023-11-14 11:33:02 -0300

    Update ej22.py

--- Tp3/ej22.py
+++ Tp3/ej22.py
@@ -1,73 +1,65 @@
-from collections import deque
+# creamos la cola
+cola_personajes = [
+    {"nombre": "Tony Stark", "superheroe": "Iron Man", "genero": "M"},
+    {"nombre": "Steve Rogers", "superheroe": "Capitán América", "genero": "M"},
+    {"nombre": "Natasha Romanoff", "superheroe": "Black Widow", "genero": "F"},
+    {"nombre": "Thor Odinson", "superheroe": "Thor", "genero": "M"},
+    {"nombre": "Bruce Banner", "superheroe": "Hulk", "genero": "M"},
+    {"nombre": "Carol Danvers", "superheroe": "Capitana Marvel", "genero": "F"},
+    {"nombre": "Scott Lang", "superheroe": "Ant-Man", "genero": "M"},
+    {"nombre": "Wanda Maximoff", "superheroe": "Scarlet Witch", "genero": "F"},
+    {"nombre": "Peter Parker", "superheroe": "Spider-Man", "genero": "M"},
+    {"nombre": "Stephen Strange", "superheroe": "Doctor Strange", "genero": "M"},
+    {"nombre": "Shuri", "superheroe": "Shuri", "genero": "F"},
+    {"nombre": "Sam Wilson", "superheroe": "Falcon", "genero": "M"},
+]
 
-class Personaje:
-    def __init__(self, nombre_personaje, nombre_superheroe, genero):
-        self.nombre_personaje = nombre_personaje
-        self.nombre_superheroe = nombre_superheroe
-        self.genero = genero
-
-# Crear una cola de personajes
-cola_personajes = deque()
-
-# Agregar personajes a la cola
-cola_personajes.append(Personaje("Tony Stark", "Iron Man", "M"))
-cola_personajes.append(Personaje("Steve Rogers", "Capitán América", "M"))
-cola_personajes.append(Personaje("Natasha Romanoff", "Black Widow", "F"))
-cola_personajes.append(Personaje("Carol Danvers", "Capitana Marvel", "F"))
-cola_personajes.append(Personaje("Scott Lang", "Ant-Man", "M"))
-cola_personajes.append(Personaje("Stephen Strange", "Doctor Strange", "M"))
-cola_personajes.append(Personaje("Wanda Maximoff", "Scarlet Witch", "F"))
-
-# Actividad a: determinar el nombre del personaje de la superhéroe Capitana Marvel
-nombre_personaje_capitana_marvel = ""
+# Actividad a. Determinar el nombre del personaje de la superhéroe Capitana Marvel
+nombre_capitana_marvel = ""
 for personaje in cola_personajes:
-    if personaje.nombre_superheroe == "Capitana Marvel":
-        nombre_personaje_capitana_marvel = personaje.nombre_personaje
-        break
+    if personaje["superheroe"] == "Capitana Marvel":
+        nombre_capitana_marvel = personaje["nombre"]
+        break  # Salir del bucle una vez encontrado
 
-print("a. El nombre del personaje de la superhéroe Capitana Marvel es:", nombre_personaje_capitana_marvel)
+print("a. Nombre del personaje de la Capitana Marvel:", nombre_capitana_marvel)
 
-# Actividad b: mostrar los nombre de los superhéroes femeninos
-print("b. Los nombres de los superhéroes femeninos son:")
+# Actividad b. Mostrar los nombres de los superhéroes femeninos
+print("b. Nombres de superhéroes femeninos:")
 for personaje in cola_personajes:
-    if personaje.genero == "F":
-        print(personaje.nombre_superheroe)
+    if personaje["genero"] == "F":
+        print(personaje["superheroe"])
 
-# Actividad c: mostrar los nombres de los personajes masculinos
-print("c. Los nombres de los personajes masculinos son:")
+# Actividad c. Mostrar los nombres de los personajes masculinos
+print("c. Nombres de personajes masculinos:")
 for personaje in cola_personajes:
-    if personaje.genero == "M":
-        print(personaje.nombre_personaje)
+    if personaje["genero"] == "M":
+        print(personaje["nombre"])
 
-# Actividad d: determinar el nombre del superhéroe del personaje Scott Lang
-nombre_superheroe_scott_lang = ""
+# Actividad d. Determinar el nombre del superhéroe del personaje Scott Lang
+nombre_scott_lang = ""
 for personaje in cola_personajes:
-    if personaje.nombre_personaje == "Scott Lang":
-        nombre_superheroe_scott_lang = personaje.nombre_superheroe
-        break
+    if personaje["nombre"] == "Scott Lang":
+        nombre_scott_lang = personaje["superheroe"]
+        break  # Salir del bucle una vez encontrado
 
-print("d. El nombre del superhéroe del personaje Scott Lang es:", nombre_superheroe_scott_lang)
+print("d. Superhéroe del personaje Scott Lang:", nombre_scott_lang)
 
-# Actividad e: mostrar todos los datos de los superhéroes o personajes cuyos nombres comienzan con la letra S
-print("e. Los superhéroes o personajes cuyos nombres comienzan con la letra S son:")
+# Actividad e. Mostrar todos los datos de los superhéroes o personajes cuyos nombres comienzan con la letra S
+print("e. Datos de superhéroes cuyos nombres comienzan con S:")
 for personaje in cola_personajes:
-    if personaje.nombre_personaje.startswith("S"):
-        print("Nombre del personaje:", personaje.nombre_personaje)
-        print("Nombre del superhéroe:", personaje.nombre_superheroe)
-        print("Género:", personaje.genero)
-        print()
+    if personaje["nombre"][0] == "S":
+        print(personaje)
 
-# Actividad f: determinar si el personaje Carol Danvers se encuentra en la cola e indicar su nombre de superhéroe
-carol_danvers_presente = False
-nombre_superheroe_carol_danvers = ""
+# Actividad f. Determinar si el personaje Carol Danvers se encuentra en la cola e indicar su nombre de superhéroe
+carol_danvers_en_cola = False
+superheroe_carol_danvers = ""
 for personaje in cola_personajes:
-    if personaje.nombre_personaje == "Carol Danvers":
-        carol_danvers_presente = True
-        nombre_superheroe_carol_danvers = personaje.nombre_superheroe
-        break
+    if personaje["nombre"] == "Carol Danvers":
+        carol_danvers_en_cola = True
+        superheroe_carol_danvers = personaje["superheroe"]
+        break  # Salir del bucle una vez encontrado
 
-if carol_danvers_presente:
-    print("f. El personaje Carol Danvers se encuentra en la cola.")
-    print("   Nombre de su superhéroe:", nombre_superheroe_carol_danvers)
+if carol_danvers_en_cola:
+    print("f. Carol Danvers está en la cola. Su superhéroe es:", superheroe_carol_danvers)
 else:
-    print("f. El personaje Carol Danvers no se encuentra en la cola.")
+    print("f. Carol Danvers no está en la cola.")
